4a1b80227d4f0af2263775cb2e5f84a50ef1dbaa
Clayton <clwht687@gmail.com>
2014-11-17 19:40:10 -0500

    Basic total and cost calculator

---
+++ addons/checkout.js
@@ -0,0 +1,47 @@
+  $('#sum').click(updateCosts());
+
+  $('#adb').click(function () {
+      add("Roman Collesum Tour", "$39.99");
+  });
+  $('#rem').click(function () {
+      $($('table .dest').children()).each(function () {
+          if ($(this).text().length > 0 && $(this).text().indexOf("Roman Collesum Tour") > -1) {
+              $(this).parent().remove();
+          }
+      });
+      updateCosts();
+
+  });
+
+  function add(dest, price) {
+      $('table .space').before('<tr class="dest"><td>' + dest + '</td><td>' + price + '</td></tr>');
+      updateCosts();
+  };
+
+
+  function remove(dest) {
+      $($('table .dest').children()).each(function () {
+          if ($(this).length > 0 && $(this).text().indexOf(dest) > -1) {
+             $(this).parents('tr').remove();
+          }
+      });
+      updateCosts();
+  }
+
+  function updateCosts() {
+      var cost = 0;
+      $($('table .dest').children()).each(function () {
+          if ($(this).text().length > 0 && $(this).text().indexOf("$") > -1) {
+              cost += parseFloat($(this).html().replace("$", ""));
+          }
+      });
+      $('#sub-total').text('$' + (cost).toFixed(2));
+      $('#tax').text('$' + (cost * .06).toFixed(2));
+      $('#total').text('$' + (cost * 1.06).toFixed(2));
+
+      if ($('table .dest').length == 0) {
+          $('table .empty').show();
+      } else {
+          $('table .empty').hide();
+      }
+  }
