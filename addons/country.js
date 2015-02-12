17d51ceb30a9871c81d32b0dba07e0f8ee86de20
Tyler A. Morris <Tmorris24@hotmail.com>
2015-02-12 10:31:08 -0500

    Moved Jquery to Country.css | Hid the CSS

--- addons/country.js
+++ addons/country.js
@@ -234,5 +234,113 @@ function clear_form(){
    });
 }
 
+$(function() {
+	 (function ($) {
+    $(document).ready(function () {
+        $('#menuToggle').click(function (e) {
+            var $parent = $(this).parent('div');
+            $parent.toggleClass('open');
+			$(".locations").toggleClass('shiftLocations');
+            var divState = $parent.hasClass('open') ? 'hide' : 'show';
+            $(this).attr('title', divState + ' divigation');
+            setTimeout(function () {
+                console.log('timeout set');
+                $('#menuToggle > span').toggleClass('divClosed').toggleClass('divOpen');
+            }, 200);
+            e.preventDefault();
+        });
+    });
+}(jQuery));
+
+
+dialog =  $("#dialog-finalize").dialog({
+	   	autoOpen: false,
+        resizable: false,
+        height: $(window).height() * .9,
+        width: $(window).width() * .85,
+        modal: true,
+        open: function(event, ui) {
+            $('.ui-widget-overlay').bind('click', function() {
+				console.log("click");
+                $("#dialog-finalize").dialog("close");
+            });
+			$("body").css('overflow', 'hidden');
+        },
+		close: function(event, ui) {
+			$("body").css('overflow', 'visible');
+		},
+        buttons: {
+            "Check Out": function() {
+               $( checkout ).dialog( "open" );
+            },
+            Cancel: function() {
+                $(this).dialog("close");
+            }
+        }
+    });
+
+   checkout =  $("#dialog-checkout").dialog({
+	   	autoOpen: false,
+        resizable: false,
+            width: 'auto',
+            height: 'auto',
+            position: {
+                my: 'center',
+                at: 'center',
+                of: window
+            },
+        modal: true,
+        open: function(event, ui) {
+            $('.ui-widget-overlay').bind('click', function() {
+				console.log("click");
+                $("#dialog-checkout").dialog("close");
+            });
+			$("body").css('overflow', 'hidden');
+        },
+		close: function(event, ui) {
+			$("body").css('overflow', 'visible');
+		},
+        buttons: {
+            "Submit Order": function() {
+	
+             if(check_form()){
+				var now = new Date();
+				var start = new Date(now.getFullYear(), 0, 0);
+				var diff = now - start;
+				
+				//86400000 is number of ms in a day
+				var day = Math.floor(diff / 86400000);
+				var mstag = now.getMilliseconds() % 1000;
+				var orderNum = day + mstag;
+				
+				var rem = "Your order #" + orderNum + " has been recieved.";
+				$(this).find(".orderRem").text(rem);
+				clear_form();
+				
+			//	$(this).dialog("options", "buttons"
+				
+				
+			//$( this ).dialog( "close" );
+			 }
+    
+
+            },
+            Cancel: function() {
+                $(this).dialog("close");
+            }
+        }
+    });
+});
+  
+  
+	$.simpleWeather({
+        location: 'Rome, Italy',
+        woeid: '',
+        unit: 'f',
+        success: weather,
+		error: function (error) {
+            $("#weather").html('<p>' + error + '</p>');
+        }
+    });
 
 
